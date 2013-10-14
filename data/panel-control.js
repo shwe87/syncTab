431b8ade3ebaece154719101fb628e1487eb5a57
shwe87 <shweta.universidad@gmail.com>
2013-10-14 18:09:34 +0200

    control the show cookies html function

--- data/panel-control.js
+++ data/panel-control.js
@@ -16,18 +16,18 @@ if(listButton != null){
 
 self.port.on('takeTabs',function(tabs){
 	console.log('Got tabs = ' + tabs);
-	var ul = document.getElementById('tabs');
+	var tabUL = document.getElementById('tabs');
 	for each (var tabsInfo in tabs){
 		var tabsURL = tabsInfo.url;
 		var tabsName = tabsInfo.title;
 		var tabsCookie = tabsInfo.cookies;
 		var tabsID = tabsInfo.id;
 		//Create a <li><a href="tab's url">tab's title</a></li>
-		var li = document.createElement('li');
+		var tabLI = document.createElement('li');
 		a = document.createElement('a');
 		href = document.createAttribute('href');	//This is an attibute of 'a'
 		href.value = tabsURL;
-		target = document.createAttribute('target');	//Open in a new widnow/tab
+		target = document.createAttribute('target');	//Open in a new window/tab
 		target.value = '_blank';
 		var style = document.createAttribute('style');
 		style.value = 'color:blue';
@@ -36,10 +36,61 @@ self.port.on('takeTabs',function(tabs){
 		a.setAttributeNode(target);
 		a.setAttributeNode(style);
 		a.appendChild(title);
-		li.appendChild(a);
-		ul.appendChild(li);
+		tabLI.appendChild(a);
+		
+		//Create a button with it's tab's ID to be able to distinguish it later
+		var b = document.createElement('BUTTON');
+		var id = document.createAttribute('id');
+		id.value = tabsID;
+		b.setAttributeNode(id);
+		var t = document.createTextNode('Cookies');
+		b.appendChild(t);
+		//Space up
+		tabLI.appendChild( document.createTextNode( '\u00A0'+'\u00A0'+'\u00A0'+'\u00A0'+'\u00A0' ) );
+		tabLI.appendChild(b);
+		
+		tabUL.appendChild(tabLI);
+	}	
+		
+	//Make a Cookie button for each tab
+	buttonsList = new Array();
+	for(i=0;i<tabs.length;i++){
+		buttonsList[i] = document.getElementById(tabs[i].id);
+		//console.log("ID " + tabs[i].id + "  " + tabs[i].title);
+		if (buttonsList[i] != null){
+			//Add an event listener for each button
+			buttonsList[i].addEventListener('click',function(event){
+				var cookieUL = document.getElementById('cookies');
+				//Clean up everything it has
+				cookieUL.innerHTML = "";
+				var pos = tabs.map(function(e) { 
+    					return e.id; 
+    				}).indexOf(this.id);
+    				var cookieCenter = document.createElement('center');
+    				var cookieH1 = document.createElement('h1');
+    				var ulText = document.createTextNode( tabs[pos].title + " Cookies:");
+    				cookieH1.appendChild(ulText);
+    				cookieCenter.appendChild(cookieH1);
+				cookieUL.appendChild(cookieCenter);
+				var cookieLI = document.createElement('li');
+				var cookieLIText = document.createTextNode(tabs[pos].cookies);
+				cookieLI.appendChild(cookieLIText);
+				cookieUL.appendChild(cookieLI);
+				/*for each (var cookie in tabs[pos].cookies){
+					var cookieLI = document.createElement('li');
+					var liText = document.createTextNode(cookie);
+					cookieLI.appendChild(liText);	
+					cookieUL.appendChild(cookieLI);			
+				}*/
+			
+			});
+			
+		
+		}
+		
 	}
 },false);
+
 
 //Hide panel when a href is clicked
 window.addEventListener('click', function(event){ 
